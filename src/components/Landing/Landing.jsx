bab56b420212398c39df0bcdefdc71e3ba7527cb
Tony Monda <ajmonda@gmail.com>
2020-11-23 17:20:37 -0500

    Scrollama function set-up for landing effect

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Scrollama, Step } from "react-scrollama";
 
-import './landing.css'
+import "./landing.css";
+
+const onStepEnter = () => {
+    console.log("enter")
+}
+
+const onStepExit = () => {
+  console.log("exit")
+}
+
 
 export default function Landing() {
   return (
     <div className="landing">
-      <h1>Hi. I'm Tony, and I love a challenge.</h1>
+      <Scrollama offset={0.4} onStepEnter={onStepEnter} onStepExit={onStepExit} debug>
+        <Step>
+          <h1>Hi. I'm Tony, and I love a challenge.</h1>
+        </Step>
+      </Scrollama>
     </div>
-  )
+  );
 }

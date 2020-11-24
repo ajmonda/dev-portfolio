3d3b4d871fefb509cfe885dbbdb0bb0aabf06069
Tony Monda <ajmonda@gmail.com>
2020-11-23 21:32:40 -0500

    Projects component tab animation on scroll

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Scrollama, Step } from "react-scrollama";
 
 import BioScroll from "../BioScroll/BioScroll";
+import Projects from "../Projects/Projects";
 
 import "./main.css";
 
 import { bio } from "../../utils/copy";
 
 export default function Main(props) {
-  return (
-    <div className="main" style={props.style}>
-      <div className="bio" style={props.bioStyle}>
-        {bio.map((bioLine, i) => {
-          return <BioScroll bioLine={bioLine} key={i} />;
-        })}
-      </div>
+  const [projectsTab, setProjectsTab] = useState(false);
 
-      <div className="content"></div>
-    </div>
+  const onStepEnter = () => {
+    setProjectsTab(!projectsTab);
+  };
+  return (
+    <>
+      <div className="main" style={props.style}>
+        <div className="bio" style={props.bioStyle}>
+          <h3>About Me</h3>
+          <Scrollama onStepEnter={onStepEnter}>
+            {bio.map((bioLine, i) => {
+              return (
+                <Step>
+                  <div>
+                    <BioScroll bioLine={bioLine} key={i} />
+                  </div>
+                </Step>
+              );
+            })}
+          </Scrollama>
+        </div>
+
+        <div className="content"></div>
+      </div>
+      <Projects style={{ top: projectsTab ? '90vh' : '100vh' }} />
+    </>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './projects.css'
+
+export default function Projects(props) {
+  return (
+    <div className="projects" style={props.style}>
+
+      <h1>Projects</h1>
+      
+    </div>
+  )
+}

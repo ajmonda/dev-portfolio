4a2d2980449a8470f3f9d37b447f527ce0d12c8a
Tony Monda <ajmonda@gmail.com>
2020-11-24 23:21:28 -0500

    Added background gradient column div

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 
-import Landing from "./components/Landing/Landing";
-import Main from "./components/Main/Main.jsx";
+import Landing from "./components/Landing";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
-import './app.css'
+// import "./app.css";
 
 export default function App() {
   return (
     <div className="app">
+      <div className="column" />
       <Landing />
-      {/* <Main /> */}
+      <Skills />
+      <Projects />
+        <Contact />
     </div>
   );
 }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+export default function Landing() {
+  return (
+    <div className="landing">
+
+      <h1>content content content</h1>
+      
+    </div>
+  )
+}

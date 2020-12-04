4eb3580819ef6f30b8660ba69d6553771dc233c0
Tony Monda <ajmonda@gmail.com>
2020-12-03 20:08:14 -0500

    Project header component

--- src/components/Header/Subtitle/Subtitle.jsx
+++ src/components/Header/Subtitle/Subtitle.jsx
@@ -5,12 +5,14 @@ import "./subtitle.css";
 export default function Subtitle() {
   return (
     <div className="subtitle">
+      <div className="subtitleContainer">
       <div className="subtitleLinks"> 
           <i className="devicon-github-plain"></i>
           <i className="devicon-linkedin-plain"></i>
         <i className="devicon-twitter-plain"></i>
       </div>
-      <h2>Full-Stack Software Engineer</h2>
+        <h2>Full-Stack Software Engineer</h2>
+        </div>
     </div>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import './projects.css'
-import Subtitle from '../Header/Subtitle/Subtitle'
+import React from "react";
+import "./projects.css";
+import ProjectHeader from "./ProjectHeader/ProjectHeader";
 
 export default function Projects(props) {
   return (
     <div className="projects" style={props.style}>
       <h3>Projects</h3>
-      <Subtitle />
-      <Subtitle />
-      <Subtitle />
+      <ProjectHeader
+        projectIcons={["devicon-react-plain"]}
+        projectTitle="Spotify Niche"
+      />
+      <ProjectHeader
+        projectIcons={["devicon-react-plain", "devicon-rails-plain", "devicon-postgresql-plain"]}
+        projectTitle="Benchmark Workout Log"
+      />
+      <ProjectHeader
+        projectIcons={[
+          "devicon-html5-plain",
+          "devicon-javascript-plain",
+        ]}
+        projectTitle="Write to Congress"
+      />
+      <ProjectHeader
+        projectIcons={[
+          "devicon-react-plain", "devicon-mongodb-plain", "devicon-express-original"
+        ]}
+        projectTitle="Eleventh Hour Rescue"
+        />
     </div>
-  )
+  );
 }

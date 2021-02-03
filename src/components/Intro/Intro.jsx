3ecddabb1e62c70434c633740f45a2234950c09f
Tony Monda <ajmonda@gmail.com>
2021-02-02 22:41:12 -0500

    Added picture of mysellf, reformatted projects section, added nav placeholders

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./intro.css";
+
+export default function Intro() {
+  return (
+    <div className="intro">
+      <div className="left">
+      <p>
+        Hi. I'm Tony, and I love a<span>&nbsp;challenge</span>.
+      </p>
+       <button className="resume-button">View My Resume</button>
+      </div>
+      
+      <img id="me" src="https://i.imgur.com/yVseRYX.png" alt="me" />
+    </div>
+  );
+}
